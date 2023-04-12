3e93bb02454cdf92a78aca59f3a22a02a3302684
Alexandre Baiocco <83550178+Alebaiocco@users.noreply.github.com>
2023-04-12 13:36:55 -0300

    login page

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,7 +1,4 @@
-// ignore_for_file: prefer_const_constructors
-
-import 'dart:html';
-
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, unused_import, avoid_unnecessary_containers
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 
@@ -16,16 +13,44 @@ class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xff000915),
-      body: Center(
+      backgroundColor: Color(0xff00000),
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/jhonMayer.png'),
+            fit: BoxFit.cover,
+            opacity: 0.40
+              )
+            ),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            SvgPicture.asset('asstes/logo.svg'),
-            //login
+          children: <Widget>[
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'E-mail',
+              ),
+            ),
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'Senha',
+              ),
+              obscureText: true,
+            ),
+            /*  RaisedButton(
+              child: Text('Entrar'),
+              onPressed: () {
+                // Adicione aqui a lógica de validação do formulário
+                if (true) {
+                  Scaffold.of(context).showSnackBar(
+                    SnackBar(
+                      content: Text('Erro de validação'),
+                    ),
+                  );
+                }
+              },
+            ), */
           ],
         ),
       ),
-    );;
+    );
   }
 }
