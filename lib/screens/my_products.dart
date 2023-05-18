c2b8b72fa9fd34e3ef08130ea5ca6a3a4913c003
Yan Julião <yanjuliao7@gmail.com>
2023-05-18 17:53:55 -0300

    myProductsPage

--- lib/screens/my_products.dart
+++ lib/screens/my_products.dart
@@ -7,12 +7,13 @@ class Product {
   final String description;
   final String garantia;
   final String photoUrl;
-  
-  Product(
-      {required this.numSerie,
-      required this.description,
-      required this.garantia,
-      required this.photoUrl,});
+
+  Product({
+    required this.numSerie,
+    required this.description,
+    required this.garantia,
+    required this.photoUrl,
+  });
 }
 
 class MyProductsPage extends StatefulWidget {
@@ -23,7 +24,7 @@ class MyProductsPage extends StatefulWidget {
 }
 
 class _MyProductsPageState extends State<MyProductsPage> {
-    final List<Product> products = [
+  final List<Product> products = [/*
     Product(
       numSerie: 08821,
       description: 'Descrição do produto 1',
@@ -41,7 +42,7 @@ class _MyProductsPageState extends State<MyProductsPage> {
       description: 'Descrição do produto 3',
       garantia: '27/12/2024',
       photoUrl: 'assets/images/pedalTres.png',
-    ),
+    ),*/
   ];
 
   @override
@@ -67,7 +68,10 @@ class _MyProductsPageState extends State<MyProductsPage> {
                   Expanded(
                     child: Text(
                       'Meus Produtos',
-                      style: TextStyle(color: Colors.white),
+                      style: TextStyle(
+                        color: Color(0xffA49930),
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
                   ),
                 ],
@@ -77,84 +81,119 @@ class _MyProductsPageState extends State<MyProductsPage> {
               thickness: 2,
               color: Color(0xffA49930),
             ),
-            ListView.builder(
-              shrinkWrap: true,
-              itemCount: products.length,
-              itemBuilder: (context, index) {
-                return Column(
-                  children: [
-                    Container(
-                      alignment: Alignment.center,
-                      margin: EdgeInsets.only(top: 10),
-                      width: 288,
-                      height: 300,
-                      color: Color(0xffA49930),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Expanded(
-                            child: Image.asset(
-                              products[index].photoUrl,
-                              fit: BoxFit.contain,
-                            ),
-                          )
-                        ],
-                      ),
-                    ),
-                    Container(
-                      alignment: Alignment.center,
-                      width: 288,
-                      height: 60,
-                      decoration: BoxDecoration(
+            if (products.isNotEmpty)
+              ListView.builder(
+                shrinkWrap: true,
+                itemCount: products.length,
+                itemBuilder: (context, index) {
+                  return Column(
+                    children: [
+                      Container(
+                        alignment: Alignment.center,
+                        margin: EdgeInsets.only(top: 20),
+                        width: 288,
+                        height: 300,
                         color: Color(0xffA49930),
-                        borderRadius: BorderRadius.only(
-                          bottomRight: Radius.circular(35),
-                        ),
-                      ),
-                      child: Column(
-                        children: [
-                          Container(
-                            margin: EdgeInsets.all(2),
-                            alignment: Alignment.topLeft,
-                            child: Text(
-                              products[index].description,
-                              style: TextStyle(
-                                  color: Colors.black,
-                                  fontFamily: 'Rubik',
-                                  fontWeight: FontWeight.bold),
-                            ),
-                          ),
-                          Container(
-                            margin: EdgeInsets.all(2),
-                            alignment: Alignment.centerLeft,
-                            child: Text(
-                              'N ${products[index].numSerie}',
-                              style: TextStyle(color: Colors.black, fontFamily: 'Rubik'),
-                            ),
-                          ),
-                          Container(
-                            margin: EdgeInsets.only(top: 2, bottom: 2, left: 2, right: 180),
-                            decoration: BoxDecoration(
-                              color: Colors.black,
-                              borderRadius: BorderRadius.only(
-                                bottomRight: Radius.circular(35),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Expanded(
+                              child: Image.asset(
+                                products[index].photoUrl,
+                                fit: BoxFit.contain,
                               ),
                             ),
-                            alignment: Alignment.bottomLeft,
-                            child: Text(
-                              products[index].garantia,
-                              style:
-                                  TextStyle(color: Color(0xffA49930), fontFamily: 'Rubik'),
-                            ),
-                          ),
-                        ],
+                          ],
+                        ),
                       ),
+                      Container(
+                        alignment: Alignment.center,
+                        width: 288,
+                        height: 55,
+                        decoration: BoxDecoration(
+                          color: Color(0xffA49930),
+                          borderRadius: BorderRadius.only(
+                            bottomRight: Radius.circular(35),
+                          ),
+                        ),
+                        child: Column(
+                          children: [
+                            Container(
+                              margin: EdgeInsets.only(left: 3),
+                              alignment: Alignment.topLeft,
+                              child: Text(
+                                products[index].description,
+                                style: TextStyle(
+                                  color: Colors.black,
+                                  fontFamily: 'Rubik',
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ),
+                            Container(
+                              margin: EdgeInsets.only(left: 3),
+                              alignment: Alignment.centerLeft,
+                              child: Text(
+                                'N ${products[index].numSerie}',
+                                style: TextStyle(
+                                  color: Colors.black,
+                                  fontFamily: 'Rubik',
+                                ),
+                              ),
+                            ),
+                            Container(
+                              margin: EdgeInsets.only(
+                                top: 2,
+                                bottom: 2,
+                                left: 3,
+                                right: 180,
+                              ),
+                              decoration: BoxDecoration(
+                                color: Colors.black,
+                                borderRadius: BorderRadius.only(
+                                  bottomRight: Radius.circular(35),
+                                ),
+                              ),
+                              alignment: Alignment.bottomLeft,
+                              child: Container(
+                                margin: EdgeInsets.only(left: 3),
+                                child: Text(
+                                  products[index].garantia,
+                                  style: TextStyle(
+                                    color: Color(0xffA49930),
+                                    fontFamily: 'Rubik',
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  );
+                },
+              )
+            else
+              Column(
+                children: [
+                  Container(
+                    margin: EdgeInsets.only(top:100, left: 100, right: 100),
+                    alignment: Alignment.center,
+                    child: Image.asset(
+                      'assets/images/empityProducts.png',
+                      height: 200,
+                      width: 200,
                     ),
-                  ],
-                );
-                // Retorna Nulo
-              },
-            ),
+                  ),
+                  Text(
+                    'Nenhum produto encontrado',
+                    style: TextStyle(
+                      color: Color(0xffA49930),
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
           ],
         ),
       ),
