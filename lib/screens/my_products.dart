cfe76c82e5b51689173d6c435fbe59cd732b6e36
Yan Julião <yanjuliao7@gmail.com>
2023-05-23 17:06:42 -0300

    Ajustes topbar, termo de garantia e manual

--- lib/screens/my_products.dart
+++ lib/screens/my_products.dart
@@ -1,8 +1,9 @@
-// ignore_for_file: prefer_const_constructors, unused_import
+// ignore_for_file: prefer_const_constructors, unused_import, deprecated_member_use, prefer_const_declarations
 
 import 'package:flutter/material.dart';
 import 'package:vttr/components/top_bar.dart';
 import 'package:url_launcher/url_launcher.dart';
+
 class Product {
   final int numSerie;
   final String description;
@@ -18,6 +19,8 @@ class Product {
 }
 
 final Uri _url = Uri.parse('https://pub.dev/packages/url_launcher');
+final String _urlDriver =
+    'https://www.tuningparts.com.br/arq/manual-tecnico-de-instalacao-fastrev01web105211072019.pdf';
 
 class MyProductsPage extends StatefulWidget {
   const MyProductsPage({Key? key}) : super(key: key);
@@ -26,11 +29,6 @@ class MyProductsPage extends StatefulWidget {
   State<MyProductsPage> createState() => _MyProductsPageState();
 }
 
-Future<void> _launchUrl() async {
-  if (!await launchUrl(_url)) {
-    throw Exception('Could not launch $_url');
-  }
-}
 class _MyProductsPageState extends State<MyProductsPage> {
   final List<Product> products = [
     Product(
@@ -60,46 +58,12 @@ class _MyProductsPageState extends State<MyProductsPage> {
       body: SingleChildScrollView(
         child: Column(
           children: [
-            TopBar(
-              text: 'Meus Produtos', 
-              text2: ''),           
+            TopBar(text: 'Meus Produtos', text2: ''),
             const Divider(
               thickness: 2,
               color: Color(0xffA49930),
             ),
             Padding(padding: EdgeInsets.only(top: 10)),
-            ElevatedButton(             
-              onPressed: () {
-                showConfirmationDialog(context);
-              }, 
-              style: ElevatedButton.styleFrom(
-                shape: RoundedRectangleBorder(
-                  side: BorderSide(
-                    width: 2,
-                    color: Color(0xffA49930),
-                  ),
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                backgroundColor:  Color(0x00A49830),
-                foregroundColor:  Color(0xffA49930),
-              ),
-              child: Text('Termos De Garantia'),
-            ),
-            ElevatedButton(             
-              onPressed: _launchUrl,
-              style: ElevatedButton.styleFrom(
-                shape: RoundedRectangleBorder(
-                  side: BorderSide(
-                    width: 2,
-                    color: Color(0xffA49930),
-                  ),
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                backgroundColor:  Color(0x00A49830),
-                foregroundColor:  Color(0xffA49930),
-              ),
-              child: Text('Manual'),
-            ),
             if (products.isNotEmpty)
               _buildProductList()
             else
@@ -117,90 +81,94 @@ class _MyProductsPageState extends State<MyProductsPage> {
       itemCount: products.length,
       itemBuilder: (context, index) {
         final product = products[index];
-        return Column(
-          children: [
-            Container(
-              alignment: Alignment.center,
-              margin: const EdgeInsets.only(top: 20),
-              width: 288,
-              height: 300,
-              color: Color(0xffA49930),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Expanded(
-                    child: Image.asset(
-                      product.photoUrl,
-                      fit: BoxFit.contain,
-                    ),
+        return GestureDetector(
+          onTap: () {
+            showManualOrDriver(context);
+          },
+          child: Column(
+            children: [
+              Container(
+                margin: const EdgeInsets.only(top: 10),
+                width: 288,
+                height: 300,
+                color: Color(0xff000915),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.only(topLeft: Radius.circular(8), topRight: Radius.circular(8)),
+                  child: Image.asset(
+                    product.photoUrl,
+                    fit: BoxFit.cover,
                   ),
-                ],
-              ),
-            ),
-            Container(
-              alignment: Alignment.center,
-              width: 288,
-              height: 55,
-              decoration: BoxDecoration(
-                color: Color(0xffA49930),
-                borderRadius: const BorderRadius.only(
-                  bottomRight: Radius.circular(35),
                 ),
               ),
-              child: Column(
-                children: [
-                  Container(
-                    margin: const EdgeInsets.only(left: 3),
-                    alignment: Alignment.topLeft,
-                    child: Text(
-                      product.description,
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontFamily: 'Rubik',
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
+              Container(
+                alignment: Alignment.center,
+                width: 288,
+                height: 55,
+                decoration: BoxDecoration(
+                  color: Color(0xffA49930),
+                  borderRadius: const BorderRadius.only(
+                    bottomRight: Radius.circular(35),
                   ),
-                  Container(
-                    margin: const EdgeInsets.only(left: 3),
-                    alignment: Alignment.centerLeft,
-                    child: Text(
-                      'N ${product.numSerie}',
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontFamily: 'Rubik',
-                      ),
-                    ),
-                  ),
-                  Container(
-                    margin: EdgeInsets.only(
-                      top: 2,
-                      bottom: 2,
-                      left: 3,
-                      right: 180,
-                    ),
-                    decoration: BoxDecoration(
-                      color: Colors.black,
-                      borderRadius: const BorderRadius.only(
-                        bottomRight: Radius.circular(35),
-                      ),
-                    ),
-                    alignment: Alignment.bottomLeft,
-                    child: Container(
+                ),
+                child: Column(
+                  children: [
+                    Container(
                       margin: const EdgeInsets.only(left: 3),
+                      alignment: Alignment.topLeft,
                       child: Text(
-                        product.garantia,
+                        product.description,
                         style: TextStyle(
-                          color: Color(0xffA49930),
+                          color: Colors.black,
+                          fontFamily: 'Rubik',
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    Container(
+                      margin: const EdgeInsets.only(left: 3),
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        'N ${product.numSerie}',
+                        style: TextStyle(
+                          color: Colors.black,
                           fontFamily: 'Rubik',
                         ),
                       ),
                     ),
-                  ),
-                ],
+                    GestureDetector(
+                      onTap: () {
+                        showConfirmationDialog(context);
+                      },
+                      child: Container(
+                        margin: const EdgeInsets.only(left: 3),
+                        alignment: Alignment.bottomLeft,
+                        child: Container(
+                          padding: EdgeInsets.symmetric(horizontal: 20),
+                          margin: EdgeInsets.only(right: 150),
+                          decoration: BoxDecoration(
+                            color: Colors.black,
+                            borderRadius: const BorderRadius.only(
+                              bottomRight: Radius.circular(35),
+                            ),
+                          ),
+                          child: Align(
+                            alignment: Alignment.centerLeft,
+                            child: Text(
+                              product.garantia,
+                              style: TextStyle(
+                                color: Color(0xffA49930),
+                                fontFamily: 'Rubik',
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         );
       },
     );
@@ -228,6 +196,7 @@ class _MyProductsPageState extends State<MyProductsPage> {
       ],
     );
   }
+
   //Função para mostrar o texto de garantia ao usuário
   void showConfirmationDialog(BuildContext context) {
     showDialog(
@@ -235,23 +204,103 @@ class _MyProductsPageState extends State<MyProductsPage> {
       builder: (BuildContext context) {
         return AlertDialog(
           title: Text('Termo de Garantia'),
-          content: Text('Aqui estão algumas informações importantes para ajudá-lo a entender melhor como a Garantia Shopee protege sua compra e como você pode solicitar o reembolso, caso haja algum contratempo com seu pedido. A Garantia Shopee assegura o recebimento e qualidade dos produtos que comprou, tornando mais fácil para você solicitar um reembolso em caso de inconvenientes. Esse processo certifica que o valor pago por você, em qualquer compra feita na nossa plataforma, seja creditado ao vendedor após 7 dias do recebimento do produto. É importante lembrar que para pedidos que ainda não foram encaminhados, os vendedores devem enviá-los dentro do período de Garantia Shopee. Caso contrário, o pagamento será automaticamente reembolsado a você após o término do prazo. Além disso, você pode usar uma extensão única de 3 dias da Garantia Shopee para um pedido que ainda não foi enviado. Para fazer isso, selecione Estender Garantia Shopee na página Detalhes do pedido.'),
+          content: Text(
+              'Aqui estão algumas informações importantes para ajudá-lo a entender melhor como a Garantia Shopee protege sua compra e como você pode solicitar o reembolso, caso haja algum contratempo com seu pedido. A Garantia Shopee assegura o recebimento e qualidade dos produtos que comprou, tornando mais fácil para você solicitar um reembolso em caso de inconvenientes. Esse processo certifica que o valor pago por você, em qualquer compra feita na nossa plataforma, seja creditado ao vendedor após 7 dias do recebimento do produto. É importante lembrar que para pedidos que ainda não foram encaminhados, os vendedores devem enviá-los dentro do período de Garantia Shopee. Caso contrário, o pagamento será automaticamente reembolsado a você após o término do prazo. Além disso, você pode usar uma extensão única de 3 dias da Garantia Shopee para um pedido que ainda não foi enviado. Para fazer isso, selecione Estender Garantia Shopee na página Detalhes do pedido.'),
           actions: [
-            TextButton(
-              onPressed: () {
-                Navigator.of(context).pop(true); // Fecha a caixa de diálogo e retorna true
-              },
-              child: Text('Confirmar'),
+            Container(
+              decoration: BoxDecoration(
+                color: Color.fromARGB(255, 111, 223, 20),
+                borderRadius: BorderRadius.circular(4),
+              ),
+              child: TextButton(
+                onPressed: () {
+                  Navigator.of(context)
+                      .pop(true); // Fecha a caixa de diálogo e retorna true
+                },
+                child: Text('Confirmar',
+                style: TextStyle(color: Colors.white)
+                ),
+              ),
             ),
-            TextButton(
-              onPressed: () {
-                Navigator.of(context).pop(false); // Fecha a caixa de diálogo e retorna false
-              },
-              child: Text('Negar'),
+            Container(
+              decoration: BoxDecoration(
+                color: Color.fromARGB(255, 236, 52, 5),
+                borderRadius: BorderRadius.circular(4),
+              ),
+              child:TextButton(
+                onPressed: () {
+                  Navigator.of(context)
+                      .pop(false); // Fecha a caixa de diálogo e retorna false
+                },
+                child: Text('Negar',
+                style: TextStyle(color: Colors.white)
+                ),
+              ),
             ),
           ],
         );
       },
     );
+  }
+
+  void showManualOrDriver(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Selecione abaixo o recurso desejado :)'),
+          actions: [
+            Container(
+              decoration: BoxDecoration(
+                color: Color(0xffA49930),
+                borderRadius: BorderRadius.circular(4),
+              ),
+              child: TextButton(
+                onPressed: () {
+                  _launchManual();
+                },
+                child: Text(
+                  'Manual',
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              decoration: BoxDecoration(
+                color: Color(0xffA49930),
+                borderRadius: BorderRadius.circular(4),
+              ),
+              child: TextButton(
+                onPressed: () {
+                  _launchDriver();
+                },
+                child: Text(
+                  'Atualização dos Drivers',
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  Future<void> _launchManual() async {
+    if (!await launchUrl(_url)) {
+      throw Exception('Could not launch $_url');
+    }
+  }
+
+  Future<void> _launchDriver() async {
+    if (await canLaunch(_urlDriver)) {
+      await launch(_urlDriver);
+    } else {
+      throw 'Não foi possível abrir o arquivo PDF';
+    }
   }
 }
