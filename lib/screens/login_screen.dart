ce363d716975be2cdcc649283b9bdcdd9123ac6d
Yan Julião <yanjuliao7@gmail.com>
2023-06-02 20:01:51 -0300

    Login Ajustes

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, unused_import, avoid_unnecessary_containers, use_key_in_widget_constructors, avoid_print, unused_local_variable, prefer_interpolation_to_compose_strings
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, unused_import, avoid_unnecessary_containers, use_key_in_widget_constructors, avoid_print, unused_local_variable, prefer_interpolation_to_compose_strings, use_build_context_synchronously, prefer_final_fields
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
@@ -6,6 +6,7 @@ import 'package:flutter_svg/svg.dart';
 import 'package:http/http.dart' as http;
 import 'package:vttr/screens/home_screen.dart';
 import 'package:vttr/screens/signup_screen.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 class Login extends StatefulWidget {
   const Login({Key? key});
@@ -17,6 +18,7 @@ class Login extends StatefulWidget {
 class _LoginState extends State<Login> {
   late TextEditingController emailController;
   late TextEditingController passwordController;
+  final _formKey = GlobalKey<FormState>();
 
   @override
   void initState() {
@@ -32,33 +34,199 @@ class _LoginState extends State<Login> {
     super.dispose();
   }
 
-  Future<String> loginUser(String email, String password) async {
-    var url = Uri.parse('http://ronaldo.gtasamp.com.br/api/user/login');
+  String? _validateEmail(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'Digite um e-mail válido';
+    }
+    final emailRegExp = RegExp(r'^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$');
+    if (!emailRegExp.hasMatch(value)) {
+      return 'Digite um e-mail válido';
+    }
+    return null;
+  }
+
+  String? _validatePassword(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'Digite uma senha válida';
+    }
+    if (value.length <= 7) {
+      return 'A senha deve conter no mínimo 8 caractéres';
+    }
+    return null;
+  }
+
+  String email = "";
+  String password = "";
+  bool isLoading = false;
+
+  Future<void> loginUser(String email, String password) async {
+    var url = Uri.parse('https://ronaldo.gtasamp.com.br/api/user/login');
+
+    List<String> listaString = [];
+
+    setState(() {
+      isLoading = true;
+    });
 
     try {
       var response = await http.post(url,
           headers: <String, String>{
-            'Content-Type': 'application/json; charset=UTF-8'
+            'Content-Type': 'application/json; charset=UTF-8',
+            "Access-Control-Allow-Credentials": 'true',
+            "Access-Control-Allow-Headers":
+                "Origin,Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,locale",
+            "Access-Control-Allow-Methods": "POST"
           },
           body: jsonEncode(
               <String, String>{'email': email, 'password': password}));
 
-      final responseBody = json.decode(response.body);
-      final data = responseBody['data'];
-      print(data);
-      if (data['token'] != null) {
-        final token = data['token'];
+      if (response.statusCode == 200) {
+        final Map<String, dynamic> data = json.decode(response.body);
 
-        print("Token = " + token);
-        return token;
+        if (data['data'] != null && data['data']['message'] != null) {
+          String message = data['data']['message'];
+
+          Fluttertoast.showToast(
+            msg: message,
+            toastLength: Toast.LENGTH_LONG,
+            gravity: ToastGravity.BOTTOM,
+            timeInSecForIosWeb: 10,
+            backgroundColor: Colors.green,
+            textColor: Colors.white,
+            webShowClose: true,
+            webPosition: 'center',
+          );
+
+          final token = data['data']['token'];
+
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => const Home()),
+          );
+        }
+      } else if (response.statusCode == 401) {
+        final Map<String, dynamic> data = json.decode(response.body);
+
+        if (data['data']['message'] != null) {
+          String errorMessage = data['data']['message'];
+
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(
+              content: Text(
+                errorMessage,
+                style: TextStyle(color: Colors.white),
+              ),
+              backgroundColor: Colors.red,
+              duration: Duration(seconds: 7),
+              action: SnackBarAction(
+                label: 'OK',
+                textColor: Colors.white,
+                onPressed: () {
+                  ScaffoldMessenger.of(context).hideCurrentSnackBar();
+                },
+              ),
+            ),
+          );
+        }
+      } else if (response.statusCode >= 500) {
+        final Map<String, dynamic> data = json.decode(response.body);
+
+        if (data['message'] != null && data['message']['erros'] != null) {
+          List<String> erros = List<String>.from(
+              data['message']['erros'].map((item) => item.toString()));
+
+          if (erros.isNotEmpty) {
+            listaString.addAll(erros);
+          }
+
+          String errorMessage = erros.join('\n');
+
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(
+              content: Text(
+                'Ops! Ocorreram alguns erros no seu cadastro:\n$errorMessage',
+                style: TextStyle(color: Colors.white),
+              ),
+              backgroundColor: Colors.red,
+              duration: Duration(seconds: 7),
+              action: SnackBarAction(
+                label: 'OK',
+                textColor: Colors.white,
+                onPressed: () {
+                  ScaffoldMessenger.of(context).hideCurrentSnackBar();
+                },
+              ),
+            ),
+          );
+        }
+      }
+        else if (response.statusCode == 401) {
+        final Map<String, dynamic> data = json.decode(response.body);
+
+        if (data['data']['message'] != null) {
+          String errorMessage = data['data']['message'];
+
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(
+              content: Text(
+                errorMessage,
+                style: TextStyle(color: Colors.white),
+              ),
+              backgroundColor: Colors.red,
+              duration: Duration(seconds: 7),
+              action: SnackBarAction(
+                label: 'OK',
+                textColor: Colors.white,
+                onPressed: () {
+                  ScaffoldMessenger.of(context).hideCurrentSnackBar();
+                },
+              ),
+            ),
+          );
+        }
+        }else {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text(
+              'Ops! Ocorreu algum erro interno',
+              style: TextStyle(color: Colors.white),
+            ),
+            backgroundColor: Colors.red,
+            duration: Duration(seconds: 7),
+            action: SnackBarAction(
+              label: 'OK',
+              textColor: Colors.white,
+              onPressed: () {
+                ScaffoldMessenger.of(context).hideCurrentSnackBar();
+              },
+            ),
+          ),
+        );
       }
     } catch (e) {
-      //Renderizar uma mensagem na tela do usuário informando que o aplicativo está fora do ar
-      print(e);
-      return "";
+      showDialog(
+        context: context,
+        barrierDismissible: false,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text('Error'),
+            content: Text(e.toString()),
+            actions: [
+              TextButton(
+                child: Text('OK'),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+            ],
+          );
+        },
+      );
+    } finally {
+      setState(() {
+        isLoading = false;
+      });
     }
-
-    return "";
   }
 
   @override
@@ -67,106 +235,103 @@ class _LoginState extends State<Login> {
       backgroundColor: Color(0xff000915),
       body: SingleChildScrollView(
         child: Container(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              Image.asset('assets/images/logo.png', width: 200, height: 200),
-              Padding(
-                padding: EdgeInsets.only(left: 10),
-                child: SizedBox(
-                  width: MediaQuery.of(context).size.width * 0.93,
-                  child: TextField(
-                    controller: emailController,
-                    style: TextStyle(color: Colors.white),
-                    decoration: InputDecoration(
-                      labelStyle: TextStyle(color: Color(0xffA49930)),
-                      labelText: 'Login',
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(5),
-                      ),
-                      enabledBorder: OutlineInputBorder(
-                        borderSide:
-                            BorderSide(width: 2, color: Color(0xffA2A2A4)),
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.only(left: 10, top: 20),
-                child: SizedBox(
-                  width: MediaQuery.of(context).size.width * 0.93,
-                  child: TextField(
-                    controller: passwordController,
-                    style: TextStyle(color: Colors.white),
-                    obscureText: true,
-                    decoration: InputDecoration(
-                      hintText: '************',
-                      labelText: 'Senha',
-                      labelStyle: TextStyle(color: Color(0xffA49930)),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(5),
-                      ),
-                      enabledBorder: OutlineInputBorder(
-                        borderSide:
-                            BorderSide(width: 2, color: Color(0xffA2A2A4)),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Image.asset('assets/images/logo.png', width: 200, height: 200),
+                Padding(
+                  padding: EdgeInsets.only(left: 10, top: 10),
+                  child: SizedBox(
+                    width: MediaQuery.of(context).size.width * 0.93,
+                    child: TextFormField(
+                      style: TextStyle(color: Colors.white),
+                      validator: _validateEmail,
+                      onChanged: (value) => email = value,
+                      decoration: InputDecoration(
+                        labelStyle: TextStyle(color: Color(0xffA49930)),
+                        labelText: 'Login',
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(5),
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderSide:
+                              BorderSide(width: 2, color: Color(0xffA2A2A4)),
+                        ),
                       ),
                     ),
                   ),
                 ),
-              ),
-              Padding(
-                padding: EdgeInsets.only(top: 5, bottom: 10),
-                child: ElevatedButton(
-                  onPressed: () {
-                    //Colocar um sninner loading
-
-                    String email = emailController.text;
-                    String password = passwordController.text;
-                    loginUser(email, password).then((value) => {
-                          //Remover spinner loading
-                          print(value),
-                          if (value.isEmpty)
-                            {
-                              print("Usuário ou senha incorreto")
-                              //Renderizar que usuário ou senha incorreto
-                            }
-                          else
-                            {
-                              //Salvar aqui o token (value) no AsyncStorage do aplicativo
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) => const Home()),
-                              )
-                            }
-                        });
-                  },
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Color(0xffA49930),
-                    foregroundColor: Colors.white,
+                Padding(
+                  padding: EdgeInsets.only(left: 10, top: 10),
+                  child: SizedBox(
+                    width: MediaQuery.of(context).size.width * 0.93,
+                    child: TextFormField(
+                      style: TextStyle(color: Colors.white),
+                      validator: _validatePassword,
+                      obscureText: true,
+                      onChanged: (value) => password = value,
+                      decoration: InputDecoration(
+                        hintText: '************',
+                        labelText: 'Senha',
+                        labelStyle: TextStyle(color: Color(0xffA49930)),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(5),
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderSide:
+                              BorderSide(width: 2, color: Color(0xffA2A2A4)),
+                        ),
+                      ),
+                    ),
                   ),
-                  child: Text("Entrar"),
                 ),
-              ),
-              Padding(
-                padding: EdgeInsets.only(top: 5, bottom: 5),
-                child: ElevatedButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => const Signup()),
-                    );
-                  },
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Color(0x00A49830),
-                    foregroundColor: Colors.white,
+                if (isLoading)
+                  Padding(
+                    padding: EdgeInsets.only(top: 80),
+                    child: CircularProgressIndicator(
+                      color: Color(0xffA49930),
+                    ),
                   ),
-                  child: Text("Cadastre-se"),
-                ),
-              ),
-            ],
+                if (!isLoading)
+                  Padding(
+                    padding: EdgeInsets.only(top: 5, bottom: 10),
+                    child: ElevatedButton(
+                      onPressed: () {
+                        if (_formKey.currentState!.validate()) {
+                          loginUser(email, password);
+                          FocusScope.of(context).unfocus();
+                        }
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Color(0xffA49930),
+                        foregroundColor: Colors.white,
+                      ),
+                      child: Text("Entrar"),
+                    ),
+                  ),
+                if (!isLoading)
+                  Padding(
+                    padding: EdgeInsets.only(top: 5, bottom: 5),
+                    child: ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const Signup()),
+                        );
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Color(0x00A49830),
+                        foregroundColor: Colors.white,
+                      ),
+                      child: Text("Cadastre-se"),
+                    ),
+                  ),
+              ],
+            ),
           ),
         ),
       ),
